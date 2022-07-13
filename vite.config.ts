9e5e7578d389cc00181919c977c19a5272aabf72
pipipi-pikachu <1171051090@qq.com>
2022-07-13 21:18:58 +0800

    add LICENSE and README

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 import components from 'unplugin-vue-components/vite'
 import { VarletUIResolver } from 'unplugin-vue-components/resolvers'
@@ -9,7 +8,6 @@ export default defineConfig({
   base: '',
   plugins: [
     vue(),
-    VitePWA({}),
     components({
       resolvers: [VarletUIResolver()]
     }),
